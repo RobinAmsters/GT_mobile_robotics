b90aa594ae85e11c3b360d9d61b2bd83f220f836
Robin <robin.amsters@kuleuven.be>
2017-10-25 18:02:28 +0200

    remove variables that are no longer needed

--- common/Odom_calc.m
+++ common/Odom_calc.m
@@ -1,24 +1,17 @@
-function [s,s_ang] = Odom_calc(x,y,ang,ds,first_time,s_set_x,s_set_y,s_set_ang)
+function [s,s_ang] = Odom_calc(ds,s_set_x,s_set_y,s_set_ang)
 %
-    s = 0
-    s_ang = 0
-    if (first_time < 0.2)               % Set the origin of axis in starting point
+    s = 0;
+    s_ang = 0;
+
         ds_x = ds.Position(1) - s_set_x;% Define x-coordinate
         ds_y = ds.Position(2) - s_set_y;% Define y-coordinate
         ds_ang = ds.Orientation(1) -s_set_ang;
-    else
-        ds_x = ds.Position(1) - x;      % Define x-coordinate
-        ds_y = ds.Position(2) - y;      % Define y-coordinate
-        ds_ang = ds.Orientation(1) - ang;
-    end
-    x = ds.Position(1);
-    y = ds.Position(2);
-    ang = ds.Orientation(1);
+
     %
     ds = sqrt((ds_x^2)+(ds_y^2));       % Real travelled distance
     %                         
-    s = s + ds                          % This ds is the travveled distance from the starting point
-    s_ang = s_ang + abs(ds_ang)
+    s = s + ds;                          % This ds is the travveled distance from the starting point
+    s_ang = s_ang + abs(ds_ang);
     if (s_ang > 6)
         s_ang = abs(2*pi - s_ang)
     end
