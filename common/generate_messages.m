37cc9394ba604ef11334cbe450cf84004447b1a5
Robin <robin.amsters@kuleuven.be>
2018-02-12 13:56:41 +0100

    add script to generate MATLAB message files

---
+++ common/generate_messages.m
@@ -0,0 +1,8 @@
+% Generate the MATLAB files neccesary for processing costum ROS messages.
+
+path = fileparts(mfilename('fullpath') ); 
+folder_path = fullfile(path) 
+rosgenmsg(folder_path)
+
+% Run the next line seperately 
+% rosmsg('show','turtlebot3_msgs/SensorState') 
